74a8d89d03c71c3bf847ba751c00ef189a6c4ffb
Paul <you@example.com>
2024-11-03 20:03:27 +0000

    Working version

--- server/src/index.js
+++ server/src/index.js
@@ -2,28 +2,126 @@ const server = require('@jspreadsheet/server');
 const openai = require('@jspreadsheet/openai');
 const adapter = require('@jspreadsheet/server-mongodb');
 const api = require('@jspreadsheet/server-api');
+const jwt = require('jsonwebtoken');
+
+// Load from ENV
+require('dotenv').config();
 
 // Jspreadsheet license
 const license = {
-    clientId: '356a192b7913b04c54574d18c28d46e6395428ab',
-    licenseKey: 'MmIyMDhmYmY4NGI1ZDY1ODAwNThjMGZkOTVkNjg2MmQ1NzZmYTFhOTBmZWI3N2M3ZmQ1N2Q3YjMwNDNhMjRhYmViYmRkNGVjZjZlMmNkNDVhODJhYzg1ZmRiY2E3OTJhYjA1ODQzNTliZGZiMmYwNWM4YmRmMjAyZmUwODA1NmEsZXlKamJHbGxiblJKWkNJNklqTTFObUV4T1RKaU56a3hNMkl3TkdNMU5EVTNOR1F4T0dNeU9HUTBObVUyTXprMU5ESTRZV0lpTENKdVlXMWxJam9pU25Od2NtVmhaSE5vWldWMElpd2laR0YwWlNJNk1UYzBNak0wTWpRd01Dd2laRzl0WVdsdUlqcGJJbXB6YUdWc2JDNXVaWFFpTENKamMySXVZWEJ3SWl3aWFuTndjbVZoWkhOb1pXVjBMbU52YlNJc0luVmxMbU52YlM1aWNpSXNJbU5rY0c0dWFXOGlMQ0pwYm5SeVlYTm9aV1YwY3k1amIyMGlMQ0p6Wm1OdlpHVmliM1F1WTI5dElpd2lkMlZpSWl3aWJHOWpZV3hvYjNOMElsMHNJbkJzWVc0aU9pSXpOQ0lzSW5OamIzQmxJanBiSW5ZM0lpd2lkamdpTENKMk9TSXNJbll4TUNJc0luWXhNU0lzSW1admNtMXpJaXdpWm05eWJYVnNZU0lzSW5KbGJtUmxjaUlzSW5CaGNuTmxjaUlzSW1sdGNHOXlkR1Z5SWl3aWRtRnNhV1JoZEdsdmJuTWlMQ0pqYjIxdFpXNTBjeUlzSW5ObFlYSmphQ0lzSW1Ob1lYSjBjeUlzSW5CeWFXNTBJaXdpWW1GeUlpd2ljMmhsWlhSeklpd2lZMnh2ZFdRaUxDSnRZWE5ySWl3aWMyaGxaWFJ6SWl3aWMyVnlkbVZ5SWl3aWFXNTBjbUZ6YUdWbGRITWlYWDA9'
+    clientId: process.env.JSS_CLIENT,
+    licenseKey: process.env.JSS_LICENSE
+};
+
+// Connect API to S3
+api({
+    s3: {
+        key: process.env.AWS_S3_KEY,
+        secret: process.env.AWS_S3_SECRET,
+        bucket: process.env.AWS_BUCKET,
+        region: process.env.AWS_S3_REGION,
+        url: process.env.AWS_S3_URL,
+    },
+    adapter: adapter
+});
+
+/**
+ * Identify if a method is part of a list of methods that requires authentication
+ * @param method
+ * @returns {boolean}
+ */
+const requireOwnership = function(method) {
+    return [
+        'setConfig',
+        'setUsers',
+        'deleteUsers',
+        'createSnapshot',
+        'restoreSnapshot',
+        'deleteSnapshot'
+    ].includes(method);
 }
 
-// MongoDB adapter
-adapter({
-    // Connect API to the mongodb adapter
-    api: api,
-    // Define the S3 information for history and image upload
-    /*s3: {
-        key: "",
-        secret: "",
-        bucket: "",
-        region: "",
-        url: "",
-    }*/
-})
+/**
+ * This method brings the level of the user
+ * @param {string} guid which document the user is trying to access
+ * @param {object} auth information about the authentication
+ * @returns {number|boolean}
+ */
+const getUserLevel = async function(guid, auth) {
+    // You must validate if the token is valid
+    let info = await jwt.decode(auth.token);
+    // Get the document information
+    let document = await adapter.get(guid);
+    if (document) {
+        // The user_id matches the subject in your JWT
+        if (info && info.sub === document.user_id) {
+            // He is the owner
+            return 2;
+        }
+        // Spreadsheet is flagged as public
+        if (! document.spreadsheet.privacy) {
+            // He has editor privileges
+            return 1;
+        }
+        // Check if there are any invitations on this document
+        if (auth.invitation) {
+            // There are invited users
+            if (document.users && document.users.length) {
+                // Search for the invitation code among invited users
+                for (let i = 0; i < document.users.length; i++) {
+                    // Match the invitation code
+                    if (document.users[i].hash === auth.invitation) {
+                        // Level is defined on the invitation 0 (readonly), 1 (editor), 2 (owner)
+                        return document.users[i].level;
+                    }
+                }
+            }
+        }
+    }
 
-// Jspreadsheet server
+    return false;
+}
+
+/**
+ * Method to check has permission to proceed
+ * @param {string} method Purpose of the request
+ * @param {string|null} guid which document user is trying to get access
+ * @param {object} auth Information about the user authentication
+ * @return {Promise<boolean>}
+ */
+const Authentication = async function(method, guid, auth) {
+    // The user is trying to load a document
+    if (guid) {
+        // Accept all formify actions
+        if (auth.origin === 'formify') {
+            return true;
+        }
+
+        // Get the user level in relation to the documented requested
+        let level = await getUserLevel(guid, auth);
+        if (level !== false) {
+            // Any invited user can connect, load. Only editors (1) or owners (2) can change the spreadsheet.
+            if (level === 0) {
+                // Readonly level, cannot change the spreadsheet
+                if (method !== 'change') {
+                    return true;
+                }
+            } else {
+                return true;
+            }
+        }
+    } else {
+        // Connection the server
+        return true;
+    }
+
+    // No access
+    return false;
+}
+
+/**
+ * Create the Jspreadsheet Server
+ */
 server({
     config: {
         cors: {
@@ -31,44 +129,91 @@ server({
         },
     },
     port: 3000,
-    error: function(e) {
-        console.error(e)
+    beforeConnect: async function(auth) {
+        // Return false to block the user to connect
+        return await Authentication('connect', null, auth);
     },
-    auth: async function(query) {
-        // Get the document
-        if (query.guid) {
-            let document = await adapter.get(query.guid);
-        }
-        return true;
+    beforeLoad: async function(guid, auth) {
+        // Return false to block the user to load a spreadsheet
+        return await Authentication('load', guid, auth);
     },
-    load: async function(guid) {
-        return await adapter.load(guid);
-    },
-    beforeChange: async function(guid, changes, query) {
-        let test = true;
-
-        // Get the document
-        if (query && query.guid) {
-            // Get document
-            let document = await adapter.get(query.guid);
-            // Check permissions
-
-            if (test === true) {
-                // Overwrite image upload to S3
-                await adapter.beforeChange(guid, changes, query);
+    beforeChange: async function(guid, changes, auth) {
+        // Before the user changes the spreadsheet
+        let test = await Authentication('change', guid, auth);
+        if (test) {
+            // Overwrite image upload and save to S3 bucket
+            if (changes.method === 'setMedia') {
+                // Make sure entries have been submitted to avoid errors
+                if (Array.isArray(changes.args[0])) {
+                    // Check any images to upload to S3
+                    for (const v of changes.args[0]) {
+                        // Found image to be uploaded
+                        if (v.src && v.src.substr(0, 5) === 'data:') {
+                            // Overwrite the URL before saving to the database
+                            let ret = await api.setImage(guid, v.src);
+                            if (ret) {
+                                v.src = ret;
+                            }
+                        }
+                    }
+                }
+            } else {
+                // Verify if the method requires ownership
+                if (requireOwnership(changes.method)) {
+                    // Requires ownership
+                    let level = await getUserLevel(guid, auth);
+                    // User is owner
+                    if (level === 2) {
+                        // For the users check if you need to send email
+                        if (changes.method === 'setUsers') {
+                            // Interface is flagged to send email
+                            let sendmail = changes.args[1];
+                            if (sendmail) {
+                                let users = changes.args[0];
+                                if (users && users.length) {
+                                    // Send the invitation by email
+                                    console.log(users)
+                                }
+                            }
+                        }
+                    } else {
+                        // The user does not have permission
+                        return false;
+                    }
+                }
             }
         }
-
         return test;
     },
-    change: async function(guid, changes, query) {
-        return await adapter.change(guid, changes, query);
+    load: async function(guid, auth, cachedConfiguration) {
+        // Load the spreadsheet when is not already in cache
+        if (! cachedConfiguration) {
+            // Load the spreadsheet from the adapter
+            cachedConfiguration = await adapter.load(guid, auth);
+        }
+        // Get the user level for this document
+        let level = await getUserLevel(guid, auth);
+        // Readonly level
+        if (level === 0) {
+            // Make sure the spreadsheet is not editable
+            cachedConfiguration.editable = false;
+        }
+        return cachedConfiguration;
     },
-    create: async function(guid, config, query) {
-        return await adapter.create(guid, config, query);
+    change: async function(guid, changes, auth, onerror) {
+        return await adapter.change(guid, changes, auth, onerror);
     },
-    destroy: async function(guid, query) {
-        return await adapter.destroy(guid, query);
+    create: async function(guid, config, auth) {
+        return await adapter.create(guid, config, auth);
+    },
+    destroy: async function(guid, auth) {
+        return await adapter.destroy(guid, auth);
+    },
+    replace: async function(guid, config, auth) {
+        return await adapter.replace(guid, config, auth);
+    },
+    error: function(e) {
+        console.error('Error', e)
     },
     license: license,
     extensions: { openai, api },
--- web/src/index.js
+++ web/src/index.js
@@ -1,8 +1,96 @@
-const express = require('express')
-const app = express()
+const jspreadsheet = require('jspreadsheet');
+const intrasheets = require('intrasheets');
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+require('jsuites/dist/jsuites.css');
+require('jspreadsheet/dist/jspreadsheet.css');
+require('intrasheets/dist/style.css');
 
-app.listen(3000)
+require('./style.css');
+
+function Sheets() {
+    // Jwt token
+    let token = localStorage.getItem('intrasheets');
+    // Get the configuration from the route
+    let path = window.location.pathname.split('/');
+    // Go to the correct route
+    if (path[1] !== 'sheets') {
+        window.location.href = '/sheets';
+    }
+
+    // Guid
+    let guid = path[2];
+    // Invitation code
+    let invitation = path[3];
+
+    // Set your JSS license key (The following key only works for one day)
+    jspreadsheet.setLicense('MTBiNWIzY2E5NzhjZTllODA2OGNmMDZlMGFkZWM2MDkxY2UwN2I0ZDlmZTdlMDQ0MjM1MDQ4N2M0ZDRjZGMxZjVlYTJhZTc0MTAyNTVlZGMxNWEyM2E0YjMzNjZkYmU0ZWQ5ODdmOGI1M2JhNTJkNWI3MTNmMTlhNDQxYzJiNTEsZXlKamJHbGxiblJKWkNJNklqTTFObUV4T1RKaU56a3hNMkl3TkdNMU5EVTNOR1F4T0dNeU9HUTBObVUyTXprMU5ESTRZV0lpTENKdVlXMWxJam9pU25Od2NtVmhaSE5vWldWMElpd2laR0YwWlNJNk1UYzJNREExTURnd01Dd2laRzl0WVdsdUlqcGJJbXB6YUdWc2JDNXVaWFFpTENKamMySXVZWEJ3SWl3aWFuTndjbVZoWkhOb1pXVjBMbU52YlNJc0ltTmtjRzR1YVc4aUxDSnBiblJ5WVhOb1pXVjBjeTVqYjIwaUxDSnpkR0ZqYTJKc2FYUjZMbU52YlNJc0luZGxZbU52Ym5SaGFXNWxjaTVwYnlJc0ltUmxkbVZzYjNBdWJHbG5hSFJ1YVc1bkxtWnZjbU5sTG1OdmJTSXNJbmRsWWlJc0lteHZZMkZzYUc5emRDSmRMQ0p3YkdGdUlqb2lNelVpTENKelkyOXdaU0k2V3lKMk55SXNJblk0SWl3aWRqa2lMQ0oyTVRBaUxDSjJNVEVpTENKbWIzSnRjeUlzSW1admNtMTFiR0VpTENKeVpXNWtaWElpTENKd1lYSnpaWElpTENKcGJYQnZjblJsY2lJc0luWmhiR2xrWVhScGIyNXpJaXdpWTI5dGJXVnVkSE1pTENKelpXRnlZMmdpTENKamFHRnlkSE1pTENKd2NtbHVkQ0lzSW1KaGNpSXNJbk5vWldWMGN5SXNJbk5vWVhCbGN5SXNJbk5sY25abGNpSXNJbVp2Y20xaGRDSXNJbWx1ZEhKaGMyaGxaWFJ6SWwxOQ==');
+
+    // Register the extension
+    jspreadsheet.setExtensions({ intrasheets });
+
+    // Start intrasheets
+    intrasheets({
+        google: {
+            clientId: 'yourClientId.apps.googleusercontent.com',
+            apiKey: 'yourApiKey',
+            redirectUri: 'http://localhost:8000/sheets',
+            errorUri: 'http://localhost:8000/sheets/error',
+        },
+        bar: {
+            suggestions: true
+        },
+        client: {
+            url: 'http://localhost:3009',
+            auth: {
+                token: token,
+                invitation: invitation,
+            },
+            onerror: function (e) {
+                console.error(e);
+            },
+            onbeforeloadimage: function (worksheet, img, options) {
+                // Image should be loaded from the API
+                if (options.src.includes('/api')) {
+                    // Intercept the images load to get those from the S3
+                    fetch('http://localhost:3009' + options.src, {
+                        headers: {
+                            'Authorization': `Bearer ${token},${invitation}`
+                        }
+                    })
+                        .then(response => {
+                            // Convert the response to a blob
+                            return response.blob();
+                        })
+                        .then(blob => {
+                            // Create and return a URL for the blob
+                            img.src = URL.createObjectURL(blob);
+                        });
+
+                    // Do not load images until the download is ready
+                    return false;
+                }
+            },
+        }
+    })
+
+    // Start application
+    if (guid && guid.length === 36) {
+        // Open the spreadsheet
+        jspreadsheet(document.getElementById('root'), {
+            guid: guid
+        });
+    } else {
+        // List all spreadsheets save for one user
+        intrasheets.list(document.getElementById('root'));
+    }
+}
+
+
+/**
+ * Create your authentication method and save the token with sub and name. You need to validate this token on your server.
+ *     Example: { sub: 1000, name: 'John Lennon' }
+ */
+localStorage.setItem('intrasheets', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMDAwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.18wUaRE2Nn389hp8pBRO898aRuhGV1V_5a3FcThBl3c')
+
+// Start intrasheets
+Sheets();
